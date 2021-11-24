a35f6155a47af6b9d9b9135d0a68026535405cb4
David Porubsky <david.porubsky@gmail.com>
2021-11-24 14:58:06 -0800

    Fixing bugs and improving alignments collapse

--- R/dotplots.R
+++ R/dotplots.R
@@ -8,11 +8,12 @@
 #' @param format Define format of the input alignment coordinates as either 'nucmer' or minimap2 'mm2', [default: 'nucmer']
 #' @param min.align.dist Keep alignment pairs with this or larger distance from each other.
 #' @param collapse.overlaps Set to \code{TRUE} to merge overlapping pair of alignments with the same relative orientation.
+#' @param title A title to be added to the final dotplot.
 #' @inheritParams paf2coords
 #' @return A \code{ggplot} object.
 #' @author David Porubsky
 #' @export
-selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, min.align.dist=1000, collapse.overlaps=TRUE) {
+selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, min.align.dist=1000, collapse.overlaps=TRUE, title=NULL) {
   ## Check if the submitted file exists
   if (!nchar(aln.coords) > 0 | !file.exists(aln.coords)) {
     stop("Submitted file in 'aln.coords' does not exists !!!")
@@ -22,7 +23,7 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
   ################
   if (format == 'nucmer') {
     ## Read in coordinates from nucmer output
-    coords <- read.table(aln.coords, skip=5, stringsAsFactors = FALSE)
+    coords <- utils::read.table(aln.coords, skip=5, stringsAsFactors = FALSE)
     coords.df <- data.frame(s1.start=coords$V1,
                             s1.end=coords$V2,
                             s2.start=coords$V4,
@@ -33,12 +34,35 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
                             s2.id=coords$V12, 
                             stringsAsFactors = FALSE)
   } else if (format == 'mm2') {
-    ## Read in coordinates from minimap2 output
+    # ## Read in coordinates from minimap2 output
+    # coords <- utils::read.table(aln.coords, stringsAsFactors = FALSE, comment.char = '&', fill = TRUE)
+    # coords.df <- data.frame(s1.start=coords$V3,
+    #                         s1.end=coords$V4,
+    #                         s2.start=coords$V8,
+    #                         s2.end=coords$V9,
+    #                         s1.width=coords$V11,
+    #                         s2.width=coords$V11,
+    #                         s1.id=coords$V1,
+    #                         s2.id=coords$V2, 
+    #                         stringsAsFactors = FALSE)
+    paf.data <- readPaf(paf.file = aln.coords, include.paf.tags = FALSE)
+    coords.df <- data.frame(s1.start=paf.data$q.start,
+                            s1.end=paf.data$q.end,
+                            s2.start=paf.data$t.start,
+                            s2.end=paf.data$t.end,
+                            s1.width=paf.data$aln.len,
+                            s2.width=paf.data$aln.len,
+                            s1.id=paf.data$q.name,
+                            s2.id=paf.data$t.name,
+                            stringsAsFactors = FALSE)
+    
+    ## Flip start and end for reverse oriented alignments
+    coords.df[paf.data$strand == '-',] <- transform(coords.df[paf.data$strand == '-',], 's2.start' = s2.end, 's2.end' = s2.start)
   }
   ## Get distance between alignments
   coords.df <- transform(coords.df, dist = abs(pmin(s2.start, s2.end) - pmax(s1.start, s1.end)))
   
-  ## Get max position
+  ## Get max position (for plotting)
   max.pos <- max(c(coords.df$s1.start, coords.df$s1.end, coords.df$s2.start, coords.df$s2.end))
   
   ## Data filtering ##
@@ -64,11 +88,19 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
   forw.mask <- (coords.df$s1.start < coords.df$s1.end) & (coords.df$s2.start < coords.df$s2.end)
   coords.df$dir[forw.mask] <- 'forw'
   
+  ## Make sure s1 coords are always smaller than s2 coords
+  toFlip <- which(pmin(coords.df$s1.start, coords.df$s1.end) > pmax(coords.df$s2.start, coords.df$s2.end))
+  coords.df[toFlip,] <- transform(coords.df[toFlip,], 's1.start' = s2.start, 's1.end' = s2.end, 's2.start' = s1.start, 's2.end' = s1.end)
+  
   ## Collapse overlapping alignments with the same directionality ##
   ##################################################################
   ## Convert alignments to Genomic ranges
   s1.gr <- GenomicRanges::GRanges(seqnames = 's1', ranges = IRanges::IRanges(start=pmin(coords.df$s1.start,coords.df$s1.end), end=pmax(coords.df$s1.start,coords.df$s1.end)), dir=coords.df$dir)
   s2.gr <- GenomicRanges::GRanges(seqnames = 's2', ranges = IRanges::IRanges(start=pmin(coords.df$s2.start,coords.df$s2.end), end=pmax(coords.df$s2.start,coords.df$s2.end)), dir=coords.df$dir)
+  ## Order by S1 coords
+  ord <- GenomicRanges::order(s1.gr)
+  s1.gr <- s1.gr[ord]
+  s2.gr <- s2.gr[ord]
   
   if (collapse.overlaps) {
     ## Get self-alignments
@@ -84,6 +116,10 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
     mask2 <- paste0(queryHits(hits2), subjectHits(hits2)) %in% paste0(queryHits(hits1), subjectHits(hits1))
     hits1 <- hits1[mask1]
     hits2 <- hits2[mask2]
+    ## Keep pairs having the same dir
+    mask <- s1.gr$dir[queryHits(hits1)] == s1.gr$dir[subjectHits(hits1)] & s2.gr$dir[queryHits(hits2)] == s2.gr$dir[subjectHits(hits2)]
+    hits1 <- hits1[mask]
+    hits2 <- hits2[mask]
     
     ## Get groups alignments overlapping each other
     groups <- list()
@@ -141,7 +177,7 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
                           s2.start = start(s2.gr),
                           s2.end = end(s2.gr),
                           s1.width = width(s1.gr),
-                          s2.width =width(s2.gr),
+                          s2.width = width(s2.gr),
                           dir = s1.gr$dir)
   ## Flip start and end for reverse oriented alignments
   coords.df[coords.df$dir == 'rev',] <- transform(coords.df[coords.df$dir == 'rev',], 's2.start' = s2.end, 's2.end' = s2.start)
@@ -160,14 +196,30 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
   ## Get polygon coordinates
   plt.dir.df <- plt.df[plt.df$dir == 'forw',]
   plt.rev.df <- plt.df[plt.df$dir == 'rev',]
-  poly.dir.df <- data.frame(x=c(rbind(plt.dir.df$s1.start, plt.dir.df$s2.start, plt.dir.df$s2.end, plt.dir.df$s1.end)),
-                            y=c(rbind(plt.dir.df$y1, plt.dir.df$y2, plt.dir.df$y1end, plt.dir.df$y2end)),
-                            group=rep(1:nrow(plt.dir.df), each=4),
-                            direction=rep(plt.dir.df$dir, each=4))
-  poly.rev.df <- data.frame(x=c(rbind(plt.rev.df$s1.start, plt.rev.df$s1.end, plt.rev.df$s2.end, plt.rev.df$s2.start)),
-                            y=c(rbind(plt.rev.df$y1, plt.rev.df$y1end, plt.rev.df$y2end, plt.rev.df$y2)),
-                            group=rep(1:nrow(plt.rev.df), each=4),
-                            direction=rep(plt.rev.df$dir, each=4))
+  if (nrow(plt.dir.df) > 0) {
+    poly.dir.df <- data.frame(x=c(rbind(plt.dir.df$s1.start, plt.dir.df$s2.start, plt.dir.df$s2.end, plt.dir.df$s1.end)),
+                              #y=c(rbind(plt.dir.df$y1, plt.dir.df$y2, plt.dir.df$y1end, plt.dir.df$y2end)),
+                              y=c(rbind(plt.dir.df$y1, plt.dir.df$y2, plt.dir.df$y2end, plt.dir.df$y1end)),
+                              group=rep(1:nrow(plt.dir.df), each=4),
+                              direction=rep(plt.dir.df$dir, each=4))
+  } else {
+    poly.dir.df <- data.frame(x=c(rbind(NaN, NaN, NaN, NaN)),
+                              y=c(rbind(NaN, NaN, NaN, NaN)),
+                              group=rep(1, each=4),
+                              direction=rep('forw', each=4))
+  }  
+  
+  if (nrow(poly.rev.df) > 0) {
+    poly.rev.df <- data.frame(x=c(rbind(plt.rev.df$s1.start, plt.rev.df$s1.end, plt.rev.df$s2.end, plt.rev.df$s2.start)),
+                              y=c(rbind(plt.rev.df$y1, plt.rev.df$y1end, plt.rev.df$y2end, plt.rev.df$y2)),
+                              group=rep(1:nrow(plt.rev.df), each=4),
+                              direction=rep(plt.rev.df$dir, each=4))
+  } else {
+    poly.rev.df <- data.frame(x=c(rbind(NaN, NaN, NaN, NaN)),
+                              y=c(rbind(NaN, NaN, NaN, NaN)),
+                              group=rep(1, each=4),
+                              direction=rep('rev', each=4))
+  }  
   
   ## Make dotplot ##
   ##################
@@ -204,6 +256,13 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
   arrow.df[,c('xmin', 'xmax')] <- t(apply(arrow.df[,c('xmin', 'xmax')], 1, sort))
   
   plt <- plt + geom_gene_arrow(data=arrow.df, aes(xmin=xmin, xmax=xmax, y=0, forward=direction, fill=dir))
+  
+  ## Add title if defined
+  if (!is.null(title)) {
+    if (nchar(title) > 0) {
+      plt <- plt + ggtitle(title)
+    }
+  }
   
   ## Return final plot
   return(plt)
