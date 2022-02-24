bd648b24d885b3d58ca31750076829b4b5528f26
David Porubsky <david.porubsky@gmail.com>
2022-02-24 13:48:54 -0800

    Adding simple dotplot function

--- R/dotplots.R
+++ R/dotplots.R
@@ -12,8 +12,8 @@
 #' @param highlight.region A pair of positions to be highlighted as vertical range.
 #' @param title A title to be added to the final dotplot.
 #' @param return Define desired output, either alignment plot as 'plot' or self-alignments in stored \code{\link{GRanges-class}} object as 'selfaln', [default: 'plot']
-#' @inheritParams paf2coords
 #' @return A \code{ggplot} object.
+#' @importFrom scales comma
 #' @author David Porubsky
 #' @export
 selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, min.align.dist=1000, collapse.overlaps=TRUE, highlight.pos=NULL, highlight.region=NULL, title=NULL, return='plot') {
@@ -260,7 +260,7 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
     geom_segment(aes(x=s2.start, xend=s2.end, y=y2, yend=y2end)) +
     geom_polygon(data=poly.dir.df, aes(x=x, y=y, group=group, fill=direction), alpha=0.25, inherit.aes=FALSE) +
     geom_polygon(data=poly.rev.df, aes(x=x, y=y, group=group, fill=direction), alpha=0.25, inherit.aes=FALSE) +
-    scale_x_continuous(limits = c(0, max.pos), labels = comma, expand = c(0, 0)) +
+    scale_x_continuous(limits = c(0, max.pos), labels = scales::comma, expand = c(0, 0)) +
     scale_y_continuous(limits = c(-1, y.limit), expand = c(0.1, 0.1)) +
     ylab('Self-alignments') +
     xlab('Contig position (bp)') +
@@ -322,4 +322,154 @@ selfdotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, mi
     warning("Please define as 'plot' or 'selfaln', returing plot by default !!!")
     return(plt)
   }
+}
+
+
+#' Plot simple dotplot of two sequences.
+#' 
+#' This function takes alignment coordinates from 'nucmer' or 'minimap2' aligner and 
+#' construct a simple dot plot.
+#'
+#' @param shape A shape used to plot aligned sequences: Either 'segm' or 'point'.
+#' @param genome.coords Set to \code{TRUE} if the target sequence should be reported in genomic coordinates.
+#' @inheritParams selfdotplot
+#' @return A \code{ggplot} object.
+#' @importFrom scales comma
+#' @author David Porubsky
+#' @export
+#' 
+simpledotplot <- function(aln.coords=NULL, format='nucmer', min.align.len=1000, highlight.pos=NULL, highlight.region=NULL, shape='segm', title=NULL, genome.coord=FALSE) {
+  
+  ## Helper function
+  remapCoord <- function(x = NULL, new.range = NULL) {
+    offset <- (min(new.range) + x[1]) - 1
+    dist <- cumsum(diff(x))
+    new.x <- c(offset, (offset + dist))
+    return(new.x)
+  } 
+  
+  ## Check if the submitted file exists
+  if (!nchar(aln.coords) > 0 | !file.exists(aln.coords)) {
+    stop("Submitted file in 'aln.coords' does not exists !!!")
+  }
+  
+  ## Data input ##
+  ################
+  if (format == 'nucmer') {
+    ## Read in coordinates from nucmer output
+    coords <- utils::read.table(aln.coords, skip=5, stringsAsFactors = FALSE)
+    coords.df <- data.frame(s1.start=coords$V1,
+                            s1.end=coords$V2,
+                            s2.start=coords$V4,
+                            s2.end=coords$V5,
+                            s1.width=abs(coords$V1 - coords$V2),
+                            s2.width=abs(coords$V4 - coords$V5),
+                            s1.id=coords$V12,
+                            s2.id=coords$V13, 
+                            stringsAsFactors = FALSE)
+  } else if (format == 'mm2') {
+    paf.data <- readPaf(paf.file = aln.coords, include.paf.tags = FALSE)
+    coords.df <- data.frame(s1.start=paf.data$q.start,
+                            s1.end=paf.data$q.end,
+                            s2.start=paf.data$t.start,
+                            s2.end=paf.data$t.end,
+                            s1.width=paf.data$aln.len,
+                            s2.width=paf.data$aln.len,
+                            s1.id=paf.data$q.name,
+                            s2.id=paf.data$t.name,
+                            stringsAsFactors = FALSE)
+    
+    ## Flip start and end for reverse oriented alignments
+    coords.df[paf.data$strand == '-',] <- transform(coords.df[paf.data$strand == '-',], 's2.start' = s2.end, 's2.end' = s2.start)
+  }
+  
+  ## Get max position (for x-axis plotting)
+  max.pos <- max(c(coords.df$s1.start, coords.df$s1.end, coords.df$s2.start, coords.df$s2.end))
+  
+  
+  ## Translate sequence coordinates to genome-wide coordinates
+  if (genome.coord) {
+    s2.region <- unique(coords.df$s2.id)
+    s2.region <- strsplit(s2.region, ":")[[1]][2]
+    s2.range <- as.numeric( strsplit(s2.region, "-")[[1]] )
+    coords.df$s2.start <- remapCoord(x = coords.df$s2.start, new.range = s2.range)
+    coords.df$s2.end <- remapCoord(x = coords.df$s2.end, new.range = s2.range)
+  }
+  
+  
+  ## Data filtering ##
+  ####################
+  ## Filter by alignment length
+  if (min.align.len > 0) {
+    coords.df <- coords.df[coords.df$s1.width >= min.align.len & coords.df$s2.width >= min.align.len,]
+  }
+  ## Remove duplicated ranges
+  xmin <- apply(coords.df[,c('s1.start', 's1.end', 's2.start', 's2.end')], 1, min)
+  xmax <- apply(coords.df[,c('s1.start', 's1.end', 's2.start', 's2.end')], 1, max)
+  coords.df <- coords.df[!duplicated(paste(xmin, xmax, sep = '_')),]
+  
+  ## Data transformations ##
+  ##########################
+  ## Add alignment directionality
+  coords.df$dir <- 'rev'
+  forw.mask <- (coords.df$s1.start < coords.df$s1.end) & (coords.df$s2.start < coords.df$s2.end)
+  coords.df$dir[forw.mask] <- 'forw'
+  
+  ## Prepare data for plotting ##
+  ###############################
+  ## Order alignments by size
+  coords.df$aln.size <- coords.df$s1.width + coords.df$s2.width
+  coords.df <- coords.df[order(coords.df$aln.size, decreasing = TRUE),]
+  
+  ## Coerce shape segment in minimap alignments are loaded
+  if (format == 'mm2') {
+    shape <- 'segm'
+    message("     Coercing shape == 'segm' for alignments in 'mm2' format!")
+  }
+  
+  if (shape == 'segm') {
+    ## Plot alignments
+    plt <- ggplot2::ggplot(coords.df, aes(x=s1.start,xend=s1.end,y=s2.start,yend=s2.end, color=dir)) + 
+      geom_segment()
+  } else if (shape == 'point') {
+    coords.df$s1.midpoint <- coords.df$s1.start + ((coords.df$s1.end - coords.df$s1.start)/2)
+    coords.df$s2.midpoint <- coords.df$s2.start + ((coords.df$s2.end - coords.df$s2.start)/2)
+    plt <- ggplot2::ggplot(coords.df, aes(x=s1.midpoint, y=s2.midpoint, color=dir)) + 
+      geom_point()
+  }  
+  plt <- plt +  
+    xlab(unique(coords.df$s1.id)) +
+    ylab(unique(coords.df$s2.id)) +
+    theme_bw() + 
+    theme(aspect.ratio=1) + #force x and y axis to have the same proportion
+    scale_color_manual(values = c('chartreuse4', 'darkgoldenrod2')) +
+    scale_x_continuous(labels = scales::comma) +
+    scale_y_continuous(labels = scales::comma)
+  
+  ## Highlight user defined positions 
+  if (!is.null(highlight.pos) & is.numeric(highlight.pos)) {
+    highlight.pos <- highlight.pos[highlight.pos > 0 & highlight.pos <= max.pos]
+    
+    if (length(highlight.pos) > 0) {
+      plt <- plt + geom_vline(xintercept = highlight.pos)
+    }  
+  }
+  
+  ## Highlight user defined region
+  if (!is.null(highlight.region)) {
+    highlight.region <- highlight.region[highlight.region$xmin > 0 & highlight.region$xmax <= max.pos,]
+    
+    if (nrow(highlight.region) > 0) {
+      plt <- plt + geom_rect(data = highlight.region, aes(xmin=xmin, xmax=xmax, ymin=0, ymax=Inf), color='red', alpha=0.25)
+    }  
+  }
+  
+  ## Add title if defined
+  if (!is.null(title)) {
+    if (nchar(title) > 0) {
+      plt <- plt + ggtitle(title)
+    }
+  }
+  ## Return final plot
+  return(plt)
 }
