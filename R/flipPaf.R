893f614966c46737c6fb0918dd2b094910c98355
David Porubsky <david.porubsky@gmail.com>
2022-11-01 10:16:26 -0700

    Adding function to flip PAF alignments

---
+++ R/flipPaf.R
@@ -0,0 +1,87 @@
+#' Flip PAF alignments
+#' 
+#' This function takes loaded PAF alignments from \function{readPaf} and flips the alignments given the desired
+#' majority strand orientation (Either '+' or '-').
+#' 
+#' @param force Set to \code{TRUE} if the PAF alignments should be flipped.
+#' @inheritParams breakPafAlignment
+#' @inheritParams syncRangesDir
+#' @return A \code{tibble} of PAF alignments flipped based on desired majority strand orientation.
+#' @importFrom tibble is_tibble as_tibble
+#' @author David Porubsky
+#' @export
+flipPaf <- function(paf.table, majority.strand=NULL, force=FALSE) {
+  ## Helper function
+  flipPafAln <- function(paf.table=NULL) {
+    paf.flip <- paf.table
+    ## Flip alignment orientation
+    paf.flip$strand[paf.table$strand == '+'] <- '-'
+    paf.flip$strand[paf.table$strand == '-'] <- '+'
+    ## Flip query coordinates
+    paf.flip$q.end <- paf.table$q.len - paf.table$q.start
+    paf.flip$q.start <- paf.table$q.len - paf.table$q.end
+    ## Make sure q.start position is always smaller than q.end
+    #q.start <- pmin(paf.flip$q.start, paf.flip$q.end)
+    #q.end <- pmax(paf.flip$q.start, paf.flip$q.end)
+    #paf.flip$q.start <- q.start
+    #paf.flip$q.end <- q.end
+    
+    return(paf.flip)
+  }
+  
+  ## Check user input ##
+  ## Make sure PAF has at least 12 mandatory fields
+  if (ncol(paf.table) >= 12) {
+    paf <- paf.table
+  } else {
+    stop('Submitted PAF alignments do not contain a minimum of 12 mandatory fields, see PAF file format definition !!!')
+  }
+  
+  ## Force alignment flip
+  if (force) {
+    paf.l <- split(paf, paf$q.name)
+    for (i in seq_along(paf.l)) {
+      paf.ctg <- paf.l[[i]]
+      paf.l[[i]] <- flipPafAln(paf.table = paf.ctg)
+    }
+    paf <- do.call(rbind, paf.l)
+  }
+  
+  ## Sync by majority strand directionality
+  if (!is.null(majority.strand) & force == FALSE) {
+    ## Define majority and minority strand
+    if (majority.strand == '+') {
+      minority.strand = '-'
+    } else if (majority.strand == '-') {
+      minority.strand = '+'
+    } else {
+      stop("Parameter 'majority.strand' can only be defined as '+' or '-' !!!")
+    }
+    
+    ## Make sure alignment is defined before deciding on majority directionality
+    if (!is.na(sum(paf$aln.len)))  {
+      if (sum(paf$aln.len) > 0) {
+        paf.l <- split(paf, paf$q.name)
+        for (i in seq_along(paf.l)) {
+          paf.ctg <- paf.l[[i]]
+          ## Flip directionality based to make sure majority strand covers the most bases
+          majority.bases <- sum(paf.ctg$aln.len[paf.ctg$strand == majority.strand])
+          minority.bases <- sum(paf.ctg$aln.len[paf.ctg$strand == minority.strand])
+          if (majority.bases < minority.bases | force) {
+            paf.l[[i]] <- flipPafAln(paf.table = paf.ctg)
+          } else {
+            paf.l[[i]] <- paf.ctg
+          }
+        }
+        paf <- do.call(rbind, paf.l)
+      }  
+    }  
+  }
+  
+  ## Export data object
+  if (tibble::is_tibble(paf)) {
+    return(paf)
+  } else {
+    return(tibble::as_tibble(paf))
+  } 
+}
