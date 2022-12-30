46fd8f0a71a2c2d7221a0afa4b0b81ef86610800
David Porubsky <david.porubsky@gmail.com>
2022-12-30 14:34:20 -0800

    Adding definition of fixed PAF column names

---
+++ R/global.R
@@ -0,0 +1,12 @@
+# =======================================================
+# Some global variables that can be used in all functions
+# =======================================================
+
+#' @import utils
+NULL
+
+## Set global variables to avoid NSE notes in R CMD check
+utils::globalVariables(c('q.name', 'q.len', 'q.start', 'q.end', 'strand',
+                         't.name', 't.len', 't.start', 't.end', 'n.match',
+                         'aln.len', 'mapq', 'aln.id', 'bin.id'))
+
