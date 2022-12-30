160e09945750b7f4383c99dac3ef634f8859ca21
David Porubsky <david.porubsky@gmail.com>
2022-12-30 14:38:11 -0800

    Adding function examples

--- R/pafToCoords.R
+++ R/pafToCoords.R
@@ -1,15 +1,30 @@
 #' Prepare PAF alignments for plotting.
-#' 
+#'
 #' This function takes loaded PAF alignments using \code{\link{readPaf}} function. Such alignment could be post-processed
 #' using \code{\link{filterPaf}}, \code{\link{breakPaf}} and \code{\link{flipPaf}} functions. Subsequently such alignments are
 #' expanded in a set of x and y coordinates ready to be plotted by \code{\link{geom_miropeats}} function.
 #'
 #' @param offset.alignments Set to \code{TRUE} if subsequent target alignments should be offsetted below and above the midline.
+#' @param add.col A user defined column name present in `paf.table` to be added in returned coordinates table.
 #' @inheritParams breakPaf
-#' @return A \code{data.frame} of PAF alignments reported as x and y coordinate values.
+#' @return A \code{tibble} of PAF alignments reported as x and y coordinate values.
+#' @importFrom tibble add_column
+#' @importFrom dplyr bind_cols
+#' @importFrom data.table ':='
 #' @author David Porubsky
 #' @export
-paf2coords <- function(paf.table, offset.alignments=FALSE) {
+#' @examples
+#'## Get PAF to process ##
+#'paf.file <- system.file("extdata", "test1.paf", package="SVbyEye")
+#'## Read in PAF
+#'paf.table <- readPaf(paf.file = paf.file, include.paf.tags = TRUE, restrict.paf.tags = 'cg')
+#'## Convert PAF alignments to x and y coordinates needed for plotting
+#'paf2coords(paf.table = paf.table)
+#'## Include optional an column to be exported as well (e.g. alignment specific GC content)
+#'paf.table$GC.content <- round(runif(nrow(paf.table), min = 30, max = 60), digits = 2)
+#'paf2coords(paf.table = paf.table, add.col = 'GC.content')
+#'
+paf2coords <- function(paf.table, offset.alignments=FALSE, add.col=NULL) {
   ## Check user input ##
   ## Make sure PAF has at least 12 mandatory fields
   if (ncol(paf.table) >= 12) {
@@ -24,16 +39,16 @@ paf2coords <- function(paf.table, offset.alignments=FALSE) {
   if (!'ID' %in% colnames(paf)) {
     paf$ID <- 'M'
   }
-  
+
   ## Flip start-end if strand == '-'
   paf[paf$strand == '-', c('t.start','t.end')] <- rev(paf[paf$strand == '-', c('t.start','t.end')])
   #paf[paf$strand == '-', c('q.start','q.end')] <- rev(paf[paf$strand == '-', c('q.start','q.end')])
-  
+
   ## Get unique alignment ID
   if (!'seq.pair' %in% colnames(paf)) {
     paf$seq.pair <- paste0(paf$q.name, '__', paf$t.name)
-  }  
-  
+  }
+
   ## Sync scales between alignments [per region id]
   paf.l <- split(paf, paf$seq.pair)
   for (i in seq_along(paf.l)) {
@@ -51,21 +66,38 @@ paf2coords <- function(paf.table, offset.alignments=FALSE) {
     # paf$q.start.trans <- q2t(x = paf$q.start, q.range = q.range, t.range = t.range)
     # paf$q.end.trans <- q2t(x = paf$q.end, q.range = q.range, t.range = t.range)
     paf.l[[i]] <- paf.sub
-  }  
+  }
   paf <- do.call(rbind, paf.l)
-  
+
   ## Vectorize data transformation ##
   x <- c(rbind(paf$q.start.trans, paf$t.start, paf$q.end.trans, paf$t.end))
   y <- rep(c(1,2,1,2), times=nrow(paf))
+
   ## Offset overlapping target alignments up&down based on start position
   if (offset.alignments) {
-    offset <- rep(c(0,0,0,0,0,0.05,0,0.05), times=ceiling(nrow(paf) / 2))[1:length(y)]
+    if ('bin.id' %in% colnames(paf)) {
+      ## Use an un-binned version of PAF alignments (group by bin ID if present)
+      to.rep <- table(paf$bin.id)
+      offset.l <- list()
+      for (i in seq_along(to.rep)) {
+        if (i %% 2 == 0) {
+          offset.l[[i]] <- rep(x = c(0,0.05,0,0.05), times = to.rep[i])
+        } else {
+          offset.l[[i]] <- rep(x = c(0,0,0,0), times = to.rep[i])
+        }
+      }
+      offset <- do.call(c, offset.l)
+    } else {
+      offset <- rep(c(0,0,0,0,0,0.05,0,0.05), times=ceiling(nrow(paf) / 2))[1:length(y)]
+    }
+    ## Add offset value to y-axis positions
     y <- y + offset
-  }  
+  }
+
   group <- rep(1:nrow(paf), each=4)
   seq.name <- c(rbind(paf$q.name, paf$t.name, paf$q.name, paf$t.name))
   seq.pos <- c(rbind(paf$q.start, paf$t.start, paf$q.end, paf$t.end))
-  seq.id <- c(rbind('query', 'target', 'query', 'target'))
+  seq.id <- rep(c('query', 'target', 'query', 'target'), times=nrow(paf))
   n.match <- rep(paf$n.match, each=4)
   aln.len <- rep(paf$aln.len, each=4)
   mapq <- rep(paf$mapq, each=4)
@@ -74,22 +106,30 @@ paf2coords <- function(paf.table, offset.alignments=FALSE) {
   seq.pair <- rep(paf$seq.pair, each=4)
   direction <- rep(paf$strand, each=4)
 
-  ## Create final data coordinate data frame
-  coords <- data.frame(x=x, 
-                       y=y, 
-                       group=group, 
-                       seq.pos=seq.pos,
-                       direction=direction,
-                       seq.name=seq.name, 
-                       seq.id=seq.id,
-                       n.match=n.match,
-                       aln.len=aln.len,
-                       mapq=mapq,
-                       aln.id=aln.id,
-                       ID=ID,
-                       #direction.flip=direction.flip,
-                       seq.pair=seq.pair,
-                       stringsAsFactors = FALSE)
+  ## Create coordinates table (for plotting)
+  coords <- dplyr::bind_cols(x = x,
+                             y = y,
+                             group = group,
+                             seq.pos = seq.pos,
+                             direction = direction,
+                             seq.name = seq.name,
+                             seq.id = seq.id,
+                             n.match = n.match,
+                             aln.len = aln.len,
+                             mapq = mapq,
+                             aln.id = aln.id,
+                             ID = ID,
+                             seq.pair = seq.pair)
+
+  ## Add user defined column name if defined
+  if (!is.null(add.col)) {
+    if (nchar(add.col) > 0 & add.col %in% colnames(paf)) {
+      new.col <- rep(paf[,eval(add.col), drop=TRUE], each=4)
+      coords <- coords %>% tibble::add_column(!!add.col := new.col)
+    } else {
+      warning("User defined column name in 'add.column' is not present in submitted 'paf.table', skipping !!!")
+    }
+  }
 
   return(coords)
 }
