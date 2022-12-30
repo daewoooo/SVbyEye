1a31997170f3cfc6868f6a184ae633b9404d8f2a
David Porubsky <david.porubsky@gmail.com>
2022-12-30 14:41:59 -0800

    Updating package and function documentation

--- R/readPaf.R
+++ R/readPaf.R
@@ -1,11 +1,11 @@
 #' Read PAF from an input file
-#' 
+#'
 #' This function takes an PAF output file from minimap2 and loads the file along
 #' with user defined set of additional alignment tags (see PAF specification).
 #'
 #' @param paf.file A path to a PAF file containing alignments to be loaded.
 #' @param include.paf.tags Set to \code{TRUE} if all additional PAF alignment tags should be included in the output.
-#' @param restrict.paf.tags Define a set of PAF tag ids (e.g. NM, cg) to be reported in the output. 
+#' @param restrict.paf.tags Define a set of PAF tag ids (e.g. NM, cg) to be reported in the output.
 #' @importFrom stringr str_split
 #' @importFrom dplyr bind_cols
 #' @importFrom S4Vectors lapply
@@ -16,8 +16,8 @@ readPaf <- function(paf.file=NULL, include.paf.tags=TRUE, restrict.paf.tags=c('N
   if (is.null(paf.file)) {
     stop("Path to a PAF file to load is not defined!!!")
   }
-  
-  ## Check if file exists and is TAB delimited 
+
+  ## Check if file exists and is TAB delimited
   if (file.exists(paf.file)) {
     con <- file(paf.file,"r")
     first.line <- readLines(con, n=1)
@@ -29,7 +29,7 @@ readPaf <- function(paf.file=NULL, include.paf.tags=TRUE, restrict.paf.tags=c('N
   } else {
     stop("User defined 'paf.file' doesn't exist!!!")
   }
-  
+
   ## Load PAF file ##
   if (file.exists(paf.file)) {
     ptm <- startTimedMessage(paste0("[readPaf] Loading PAF file: ", paf.file))
@@ -42,14 +42,14 @@ readPaf <- function(paf.file=NULL, include.paf.tags=TRUE, restrict.paf.tags=c('N
     }
     paf.fields <- S4Vectors::lapply(fields, "[", 1:12)
     field.names <-  c('q.name', 'q.len', 'q.start', 'q.end', 'strand', 't.name', 't.len', 't.start', 't.end', 'n.match', 'aln.len', 'mapq')
-    
+
     for (i in seq_along(paf.fields)) {
       attr(paf.fields[[i]], "names") <- field.names
     }
     paf <- dplyr::bind_rows(paf.fields)
     cols.num <- c(2, 3, 4, 7:12)
     paf[cols.num] <- suppressWarnings( dplyr::bind_cols(S4Vectors::lapply(paf[cols.num], as.numeric)) )
-    
+
     if (include.paf.tags) {
       if (any(lengths(fields) > 12)) {
         paf.tags <- S4Vectors::lapply(fields, function(x) paste(x[13:length(x)]))
@@ -61,8 +61,8 @@ readPaf <- function(paf.file=NULL, include.paf.tags=TRUE, restrict.paf.tags=c('N
   } else {
     stop(paste0("PAF file ", paf.file, " doesn't exists !!!"))
     return(NULL)
-  }  
-}  
+  }
+}
 
 #' Process PAF specific alignment tags.
 #'
@@ -80,7 +80,7 @@ processPafTags <- function(paf.tags, restrict.paf.tags=c('NM', 'cg')) {
     message(paste0("Submitted 'restrict.paf.tags' are not present in the allowed set of PAF tags: ", paste(allowed.tags, collapse = '; ')))
     restrict.paf.tags <- NULL
   }
-  
+
   tags <- character()
   to.numeric <- integer()
   res <- list()
@@ -99,10 +99,10 @@ processPafTags <- function(paf.tags, restrict.paf.tags=c('NM', 'cg')) {
           tags <- c(tags, tag[1])
         }
         res[[ tag[1] ]][ali.idx] <- tag[3]
-      }  
+      }
     }
   }
-  
+
   for(i in to.numeric){
     res[[i]] <- as.numeric(res[[i]])
   }
