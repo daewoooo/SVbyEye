8370677e741d957dcc9994a4b9c622a342a9747f
David Porubsky <david.porubsky@gmail.com>
2023-02-07 09:56:48 -0800

    Fixing bugs in query2target coordinates lifting

--- R/liftRangesToAlignment.R
+++ R/liftRangesToAlignment.R
@@ -27,62 +27,89 @@ liftRangesToAlignment <- function(gr = NULL, paf.file = NULL, direction = "query
     ## Make sure defined ranges are present in the PAF alignment
     if (direction == "query2target") {
         paf.query.gr <- GenomicRanges::GRanges(seqnames = paf.aln$q.name, ranges = IRanges::IRanges(start = paf.aln$q.start, end = paf.aln$q.end))
-        hits <- suppressWarnings(GenomicRanges::findOverlaps(gr, paf.query.gr))
+        ## Optional step to get only overlapping ranges [To be tested]
+        #gr <- GenomicRanges::pintersect(paf.query.gr, gr)
+        #gr <- gr[gr$hit == TRUE]
+        hits <- suppressWarnings(GenomicRanges::findOverlaps(gr, paf.query.gr, ignore.strand=TRUE))
+        from <- 'query'
     } else if (direction == "target2query") {
         paf.target.gr <- GenomicRanges::GRanges(seqnames = paf.aln$t.name, ranges = IRanges::IRanges(start = paf.aln$t.start, end = paf.aln$t.end))
-        hits <- suppressWarnings(GenomicRanges::findOverlaps(gr, paf.target.gr))
+        hits <- suppressWarnings(GenomicRanges::findOverlaps(gr, paf.target.gr, ignore.strand=TRUE))
+        from <- 'target'
     } else {
         stop("Parameter 'direction' can take only values: 'query2target' or 'target2query'")
     }
 
     if (length(hits) == 0) {
-        message("None of the user defined ranges are present in the PAF query coordinates!!!")
+        message(paste0("None of the user defined ranges are present in the PAF ", from, " coordinates!!!"))
         ## Return zero positioned ranges if there is no overlap
         gr.lifted <- GenomicRanges::GRanges(seqnames = rep("Failed", length(gr)), ranges = IRanges::IRanges(start = 1, end = 0))
         return(gr.lifted)
     } else {
         ## Lift ranges to PAF alignment
         if (direction == "query2target") {
-            ## Set names to ranges to be lifted
-            gr.lift <- gr[S4Vectors::queryHits(hits)]
-            names(gr.lift) <- paste0("aln", S4Vectors::subjectHits(hits))
-            ## Define PAF alignment object
-            paf.aln <- paf.aln[unique(S4Vectors::subjectHits(hits)), ]
-            alignment <- GenomicAlignments::GAlignments(
-                seqnames = paf.aln$t.name,
-                pos = as.integer(paf.aln$t.start) + 1L,
-                cigar = paf.aln$cg,
-                strand = GenomicRanges::strand(paf.aln$strand),
-                names = paste0("aln", seq_len(nrow(paf.aln)))
-            )
-            gr.lifted <- GenomicAlignments::mapFromAlignments(x = gr.lift, alignments = alignment)
-            names(gr.lifted) <- NULL
-        } else {
-            gr.lift <- gr
-            ## Define PAF alignment object
-            paf.aln <- paf.aln[unique(S4Vectors::subjectHits(hits)), ]
-            alignment <- GenomicAlignments::GAlignments(
-                seqnames = paf.aln$t.name,
-                pos = as.integer(paf.aln$t.start) + 1L,
-                cigar = paf.aln$cg,
-                strand = GenomicRanges::strand(paf.aln$strand),
-                names = paf.aln$q.name
-            )
+          ## Map from alignment ##
+          gr.lift <- gr[queryHits(hits)]
+          names(gr.lift) <- paste0("aln", S4Vectors::subjectHits(hits))
+          ## Define Genomic alignments object
+          paf.aln <- paf.aln[unique(S4Vectors::subjectHits(hits)),]
+          alignment <- GenomicAlignments::GAlignments(
+            seqnames = paf.aln$t.name,
+            pos = as.integer(paf.aln$t.start) + 1L,
+            #pos = pos,
+            cigar = paf.aln$cg,
+            strand = GenomicRanges::strand(paf.aln$strand),
+            names = paste0("aln", unique(S4Vectors::subjectHits(hits)))
+          )
+          ## Flip desired coordinates in case of minus alignments and adjust to alignment bounds
+          if (any(as.character(GenomicRanges::strand(alignment)) == "-")) {
+            ## Get alignments to flip
+            aln.idx <- which(as.character(GenomicRanges::strand(alignment)) == "-")
+            bounds <- IRanges::IRanges(start = 1L, end = paf.aln$q.end[aln.idx])
+            mask <- which(names(gr.lift) %in% names(alignment)[aln.idx])
+            ## Flip reverse alignments
+            IRanges::ranges(gr.lift[mask]) <- IRanges::reflect(x = IRanges::ranges(gr.lift[mask]), bounds = bounds)
+          }
+          ## Adjust start position for plus alignments alignment
+          if (any(as.character(GenomicRanges::strand(alignment)) == "+")) {
+            ## Get alignments to flip
+            aln.idx <- which(as.character(GenomicRanges::strand(alignment)) == "+")
+            mask <- which(names(gr.lift) %in% names(alignment)[aln.idx])
+            ## Adjust start position of plus alignments to a local alignment coordinates
+            start(gr.lift[mask]) <- abs( start(gr.lift[mask]) - paf.aln$q.start[aln.idx] )
+            end(gr.lift[mask]) <- abs( end(gr.lift[mask]) - paf.aln$q.start[aln.idx] )
+          }
 
-            gr.lifted <- GenomicAlignments::mapToAlignments(x = gr.lift, alignments = alignment)
-            names(gr.lifted) <- NULL
-            ## Flip coordinates in case of reverse alignment
-            if (any(S4Vectors::runValue(GenomicRanges::strand(alignment)) == "-")) {
-                # start.reverse <- (paf.aln$q.end[gr.lifted$alignmentsHits] - end(gr.lifted)) + 1
-                # end.reverse <- (paf.aln$q.end[gr.lifted$alignmentsHits] - start(gr.lifted)) + 1
-                # ranges(gr.lifted) <- IRanges::IRanges(start=start.reverse, end=end.reverse)
-                ## Get alignments to flip
-                aln.id <- which(S4Vectors::runValue(GenomicRanges::strand(alignment)) == "-")
-                bounds <- IRanges::IRanges(start = 1L, end = paf.aln$q.end[aln.id])
-                mask <- gr.lifted$alignmentsHits == aln.id
-                ## Flip reverse alignments
-                IRanges::ranges(gr.lifted[mask]) <- IRanges::reflect(x = IRanges::ranges(gr.lifted[mask]), bounds = bounds)
-            }
+          gr.lifted <- GenomicAlignments::mapFromAlignments(x = gr.lift, alignments = alignment)
+          names(gr.lifted) <- NULL
+
+        } else {
+          ## Map to alignment ##
+          gr.lift <- gr
+          ## Define PAF alignment object
+          paf.aln <- paf.aln[unique(S4Vectors::subjectHits(hits)), ]
+          alignment <- GenomicAlignments::GAlignments(
+            seqnames = paf.aln$t.name,
+            pos = as.integer(paf.aln$t.start) + 1L,
+            cigar = paf.aln$cg,
+            strand = GenomicRanges::strand(paf.aln$strand),
+            names = paf.aln$q.name
+          )
+          gr.lifted <- GenomicAlignments::mapToAlignments(x = gr.lift, alignments = alignment)
+          names(gr.lifted) <- NULL
+
+          ## Flip coordinates in case of reverse alignment
+          if (any(S4Vectors::runValue(GenomicRanges::strand(alignment)) == "-")) {
+            # start.reverse <- (paf.aln$q.end[gr.lifted$alignmentsHits] - end(gr.lifted)) + 1
+            # end.reverse <- (paf.aln$q.end[gr.lifted$alignmentsHits] - start(gr.lifted)) + 1
+            # ranges(gr.lifted) <- IRanges::IRanges(start=start.reverse, end=end.reverse)
+            ## Get alignments to flip
+            aln.id <- which(S4Vectors::runValue(GenomicRanges::strand(alignment)) == "-")
+            bounds <- IRanges::IRanges(start = 1L, end = paf.aln$q.end[aln.id])
+            mask <- gr.lifted$alignmentsHits == aln.id
+            ## Flip reverse alignments
+            IRanges::ranges(gr.lifted[mask]) <- IRanges::reflect(x = IRanges::ranges(gr.lifted[mask]), bounds = bounds)
+          }
         }
 
         ## Add index corresponding to original input ranges
